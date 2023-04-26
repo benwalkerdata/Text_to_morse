ba06bf38acc55afc8e08d1433cd1306681d1efe2
benwalkerit <benrwalker@icloud.com>
2023-04-26 11:27:42 +0100

    morse to text script complete.

---
+++ Day 81 - Text to Morse Code/main.py
@@ -0,0 +1,59 @@
+#Morse code dictionary
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '/':'-..-.', '-':'-....-',
+                    '(':'-.--.', ')':'-.--.-'}
+
+#Function to encrypt the string.
+
+def encrypt(message):
+    cipher = ''
+    for letter in message:
+        if letter != ' ':
+            cipher += MORSE_CODE_DICT[letter] + ' '
+        else:
+            cipher += ' '
+    return cipher
+
+#Function to decrypt the string.
+def decrypt(message):
+    message += ' '
+    decipher = ''
+    citext = ''
+    for letter in message:
+        if (letter != ' '):
+            i = 0
+            citext +=letter
+        else:
+            i += 1
+            if i == 2:
+                decipher += ' '
+            else:
+                decipher += list(MORSE_CODE_DICT.keys())[list(MORSE_CODE_DICT.values()).index(citext)]
+                citext = ''
+    return decipher
+
+#Main function with option encrypt/decrypt and input the message.
+def main():
+    encrypt_decrypt = int(input("Do you wan to encrypt or decrypt a message? 1 for encrypt, 2 for decrypt? "))
+    message = input("Please enter you message: ").upper()
+    if encrypt_decrypt == 1:
+        result = encrypt(message)
+        print(result)
+    elif encrypt_decrypt == 2:
+        result = decrypt(message)
+        print(result)
+        
+if __name__ == '__main__':
+    main()
